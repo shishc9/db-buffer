2132bfa16cd33683f0710f50d500d856495db8a9
shishc9 <52554717+shishc9@users.noreply.github.com>
2022-04-22 15:16:52 +0800

    fix: exception package

--- src/disk/DBFile.java
+++ src/disk/DBFile.java
@@ -1,17 +1,13 @@
 package disk;
 
+import exception.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Arrays;
 
 public class DBFile {
-
-    public static class PageNotAllocatedException extends RuntimeException {}
-    public static class EmptyFileException extends RuntimeException {}
-    public static class IllegalPageNumberException extends RuntimeException {}
-    public static class NonPositiveRunSizeException extends RuntimeException {}
-    public static class FileFullException extends RuntimeException {}
 
     private String dataFileName;
     private String mapFileName;
@@ -65,7 +61,14 @@ public class DBFile {
         return isDelete;
     }
 
+    /**
+     * 分配指定大小的数据页，并返回第一个可用的页号.
+     * 文件空间的分配策略不会使文件产生碎片.
+     * @param runSize 指定大小.
+     * @return 第一个可用的页号.
+     */
     public Integer allocatePages(Integer runSize) throws IOException{
+        // 参数检查
         if (runSize <= 0) {
             throw new NonPositiveRunSizeException();
         }
@@ -77,13 +80,14 @@ public class DBFile {
 
         for (int i = 0; i < mapFile.length() - runSize + 1; i++) {
             int currentRunSize = 0;
+            // 从当前位置开始，偏移量为runSize，找一段连续且为未被分配的空间.
             for (int j = i; j < i + runSize; j++) {
                 if (map[j] != 0) {
                     break;
                 }
                 currentRunSize ++;
             }
-
+            // 已经找到了.
             if (currentRunSize == runSize) {
                 byte[] mapUpdate = new byte[runSize];
                 Arrays.fill(mapUpdate, (byte) 1);
@@ -94,11 +98,19 @@ public class DBFile {
             }
         }
 
+        // 若未找到连续且runSize大小的空间，说明文件已满.
         mapFile.close();
         throw new FileFullException();
     }
 
+    /**
+     * 写入页号为pageNum的页面page.
+     * @param pageNum 页号
+     * @param page 页面数据
+     * @throws IOException
+     */
     public void writePage(Integer pageNum, Page page) throws IOException{
+        // 参数检验
         if (numPages == 0) {
             throw new EmptyFileException();
         }
@@ -110,11 +122,13 @@ public class DBFile {
         mapFile.seek(pageNum);
         byte[] pagePos = new byte[1];
         mapFile.readFully(pagePos);
+        // 当前页号还未被分配.
         if (pagePos[0] == 0) {
             throw new PageNotAllocatedException();
         }
         mapFile.close();
 
+        // 写入页面.
         RandomAccessFile dataFile = new RandomAccessFile(dataFileName, "rw");
         dataFile.seek(pageNum * Page.PAGE_SIZE);
         dataFile.write(page.data);
---
+++ src/exception/IllegalPageNumberException.java
@@ -0,0 +1,4 @@
+package exception;
+
+public class IllegalPageNumberException extends RuntimeException{
+}
